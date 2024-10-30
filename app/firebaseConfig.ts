f14b7a72aba256496b6c7a61c05e668cbcbe3bec
Malli7 <mallireddy2001@gmail.com>
2024-10-30 11:19:08 -0500

    changes to firebase config

--- app/firebaseConfig.ts
+++ app/firebaseConfig.ts
@@ -1,15 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from 'firebase/database';
+import 'dotenv/config';
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCwYMECiASJyaGCSkDWzSmzJv-JUBGcF68",
-  authDomain: "my-chat-app-214c0.firebaseapp.com",
+  apiKey:process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
   databaseURL: "https://my-chat-app-214c0-default-rtdb.firebaseio.com",
-  projectId: "my-chat-app-214c0",
-  storageBucket: "my-chat-app-214c0.appspot.com",
-  messagingSenderId: "52024154303",
-  appId: "1:52024154303:web:52109a2159ec8fd1370e0a",
-  measurementId: "G-MPQ69JVESP"
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId:process.env.FIREBASE_MESSAGINGSENDERID,
+  appId:process.env.FIREBASE_APPID,
+  measurementId: process.env.FIREBASE_MEASUREMENTID
 };
 
 const app = initializeApp(firebaseConfig);
